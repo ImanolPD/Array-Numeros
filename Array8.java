f3df5a4030babbc7e51a9f2bbe1f5c18e2d53ce5
Ima <145325107+ImanolPD@users.noreply.github.com>
2023-12-19 12:14:13 +0100

    Add files via upload

--- Array8.java
+++ Array8.java
@@ -1,4 +1,23 @@
-
 public class Array8 {
 
+    public static void main(String[] args) {
+        // Declarar e inicializar el array
+        int[] tabla = {3, 6, 2, 77, 3, 10, 23, 14};
+
+        // Verificar si la secuencia es la misma al leer de izquierda a derecha y de derecha a izquierda
+        boolean esIgual = true;
+
+        for (int i = 0; i < tabla.length / 2; ++i) {
+            if (tabla[i] != tabla[tabla.length - 1 - i]) {
+                esIgual = false;
+                break;
+            }
+        }
+
+        if (esIgual) {
+            System.out.println("La secuencia es la misma al leer de izquierda a derecha y de derecha a izquierda.");
+        } else {
+            System.out.println("La secuencia no es la misma al leer de izquierda a derecha y de derecha a izquierda.");
+        }
+    }
 }
